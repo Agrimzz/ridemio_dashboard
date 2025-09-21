115ddc91103f66b0bf1242bbcecc00db7f9f4f20
Agrim Pradhananga <agrimp91@gmail.com>
2025-09-21 13:00:01 +0545

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,48 @@
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import "@mantine/core/styles.css";
+
+import {
+  ColorSchemeScript,
+  MantineProvider,
+  colorsTuple,
+  createTheme,
+  mantineHtmlProps,
+} from "@mantine/core";
+
+export const metadata = {
+  title: "My Mantine app",
+  description: "I have followed setup instructions carefully",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const theme = createTheme({
+    fontFamily: '"Manrope", sans-serif',
+    colors: {
+      background: colorsTuple("#FBFAFC"),
+      primary: colorsTuple("#5542F6"),
+      success: colorsTuple("#20C9AC"),
+      danger: colorsTuple("#FC3400"),
+      info: colorsTuple("#00A5FF"),
+      textPrimary: colorsTuple("#2E2C34"),
+      textSecondary: colorsTuple("#84818A"),
+      grayDark: colorsTuple("#D3D4D5"),
+    },
+    primaryColor: "primary",
+  });
+
+  return (
+    <html lang="en" {...mantineHtmlProps}>
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body>
+        <MantineProvider theme={theme}>{children}</MantineProvider>
+      </body>
+    </html>
+  );
+}
